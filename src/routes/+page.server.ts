34a9f1502dcf9a68152e27b3307d81065bcae130
Yousef <yousefjavaherian@gmail.com>
2025-03-03 20:51:04 +0330

    creating a better homepage

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,10 @@
-import { createProject } from '$lib/server/actions';
+import { createProject, getProjects } from '$lib/server/actions';
 import type { Actions } from './$types';
+
+export async function load() {
+    const projects = await getProjects()
+    return { projects }
+}
 
 export const actions: Actions = {
     create: async ({ request }) => {
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,28 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+    import { browser } from "$app/environment";
+
+    let { data } = $props();
+    const projects = data.projects;
+    if (projects.length > 0 && browser) {
+        goto(`/project/${projects[projects.length - 1].id}`);
+    }
+</script>
+
 <h1 class="text-4xl">Pocket watch</h1>
-<p>
-    👈 please select or create a project from the side menu to start the
-    application
-</p>
+<main>
+    <p>
+        Pocket watch is a time tracking app that helps you keep track of your
+        time and bill your clients.
+    </p>
+    <p>
+        you have not created any project yet, fill the form below to add a new
+        project:
+    </p>
+    <form action="?/create" method="post" class="">
+        <label>
+            <input type="text" name="name" id="name" />
+        </label>
+        <button type="submit" class="px-2">+</button>
+    </form>
+</main>
