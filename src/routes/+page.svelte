51388fe8c6f1e04bab86dd66d86e73d3e9db9163
Yousef <yousefjavaherian@gmail.com>
2025-03-03 22:24:07 +0330

    in the mist of creating a new UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import { FolderPlus } from "lucide-svelte";
     import { goto } from "$app/navigation";
     import { browser } from "$app/environment";
 
@@ -9,20 +10,35 @@
     }
 </script>
 
-<h1 class="text-4xl">Pocket watch</h1>
-<main>
-    <p>
+<h1
+    class="text-4xl flex flex-row items-end p-4 bg-amber-200 font-[Doto] font-black text-amber-950border-b"
+>
+    <img src="/favicon.png" alt="logo" class="w-12 mr-2" />
+    Pocket watch
+</h1>
+<main class="m-4">
+    <h2 class="text-xl font-bold">
         Pocket watch is a time tracking app that helps you keep track of your
         time and bill your clients.
-    </p>
-    <p>
+    </h2>
+    <p class="mt-4">
         you have not created any project yet, fill the form below to add a new
-        project:
+        project 👇:
     </p>
     <form action="?/create" method="post" class="">
         <label>
-            <input type="text" name="name" id="name" />
+            Project Name:
+            <input
+                type="text"
+                name="name"
+                id="name"
+                class="bg-amber-100 rounded px-2 py-1"
+            />
         </label>
-        <button type="submit" class="px-2">+</button>
+        <button
+            type="submit"
+            class="bg-amber-200 rounded px-2 py-1 hover:bg-amber-300"
+            >New Project <FolderPlus class="ml-1 inline" />
+        </button>
     </form>
 </main>
--- src/routes/project/[id]/+layout.svelte
+++ src/routes/project/[id]/+layout.svelte
@@ -1,29 +1,52 @@
 <script>
     import { page } from "$app/state";
+    import { FolderPlus, SquareCheckBig } from "lucide-svelte";
     let { children, data } = $props();
 </script>
 
-<main class="flex flex-row">
-    <div class="flex flex-col">
-        {#each data.projects as project}
-            <a
-                href="/project/{project.id}/"
-                class={[
-                    (page.params.id ?? null) == project.id.toString() &&
-                        "bg-stone-200",
-                ]}
+<div class="h-svh flex flex-col">
+    <h1
+        class="text-4xl flex flex-row items-end p-4 bg-amber-200 font-[Doto] font-black text-amber-950border-b"
+    >
+        <img src="/favicon.png" alt="logo" class="w-12 mr-2" />
+        Pocket watch
+    </h1>
+    <main class="flex flex-row m-4 grow">
+        <div class="flex flex-col w-60">
+            {#each data.projects as project}
+                <a
+                    href="/project/{project.id}/"
+                    class={[
+                        "py-1 px-2 hover:bg-stone-50 active:bg-stone-100 rounded-md",
+                        (page.params.id ?? null) == project.id.toString() &&
+                            "bg-amber-100",
+                    ]}
+                >
+                    {project.name}
+                </a>
+            {/each}
+            <form
+                action="?/create"
+                method="post"
+                class="flex flex-row items-center rounded-md bg-amber-100 py-1 px-2 fixed bottom-8"
             >
-                {project.name}
-            </a>
-        {/each}
-        <form action="?/create" method="post" class="border">
-            <label>
-                <input type="text" name="name" id="name" />
-            </label>
-            <button type="submit" class="px-2">+</button>
-        </form>
-    </div>
-    <div>
-        {@render children()}
-    </div>
-</main>
+                <label>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        class=""
+                        placeholder="project name ..."
+                        autocomplete="off"
+                    />
+                </label>
+                <button type="submit" class="text-amber-400 h-8"
+                    ><FolderPlus /></button
+                >
+            </form>
+        </div>
+        <div class="ml-4">
+            {@render children()}
+        </div>
+    </main>
+</div>
