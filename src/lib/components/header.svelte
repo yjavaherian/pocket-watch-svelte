1569aee08b696d01c80d67b22fd856b814a92ad9
Yousef <yousefjavaherian@gmail.com>
2025-03-11 14:30:51 +0330

    formatting

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-    import Github from "$lib/icons/github.svelte";
+  import Github from "$lib/icons/github.svelte";
 </script>
 
-<h1 class="flex flex-row p-4 border-b border-stone-500 items-end">
-    <img src="/favicon.png" alt="logo" class="w-12 mr-2" />
-    <span class="text-4xl font-[Doto] font-black text-pampas-950">
-        Pocket watch
-    </span>
-    <a
-        href="https://github.com/yjavaherian/pocket-watch-svelte"
-        class="ml-auto self-center"
-    >
-        <Github class="h-8 w-8" />
-    </a>
+<h1 class="flex flex-row items-end border-b border-stone-500 p-4">
+  <img src="/favicon.png" alt="logo" class="mr-2 w-12" />
+  <span class="text-pampas-950 font-[Doto] text-4xl font-black">
+    Pocket watch
+  </span>
+  <a
+    href="https://github.com/yjavaherian/pocket-watch-svelte"
+    class="ml-auto self-center"
+  >
+    <Github class="h-8 w-8" />
+  </a>
 </h1>
--- src/lib/components/project-header.svelte
+++ src/lib/components/project-header.svelte
@@ -19,22 +19,22 @@
         class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-50 bg-gray-900/80"
       />
       <Dialog.Content
-        class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 outline-hidden fixed left-[50%] top-[50%] z-50 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] p-5 sm:max-w-[490px] md:w-full"
+        class="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] p-5 outline-hidden sm:max-w-[490px] md:w-full"
       >
         <Paper class="px-8 py-4">
           <span>
             Are you sure you want to delete project "{project.name}"?
           </span>
-          <form action="?/delete" method="post" class="block ml-auto mt-10">
+          <form action="?/delete" method="post" class="mt-10 ml-auto block">
             <input type="hidden" name="id" value={project.id} />
             <Dialog.Close
-              class="w-18 rounded p-1 border bg-sand-50 hover:bg-sand-200 border-stone-500"
+              class="bg-sand-50 hover:bg-sand-200 w-18 rounded border border-stone-500 p-1"
             >
               Cancel
             </Dialog.Close>
             <button
               type="submit"
-              class="w-18 rounded p-1 bg-eunry-500 hover:bg-eunry-600 text-sand-50"
+              class="bg-eunry-500 hover:bg-eunry-600 text-sand-50 w-18 rounded p-1"
               onclick={() => (open = false)}
             >
               Delete
@@ -49,37 +49,37 @@
     <h1 class="text-3xl font-bold capitalize">{project.name}</h1>
     <DropdownMenu.Root>
       <DropdownMenu.Trigger class="ml-auto">
-        <Paper class="rounded p-1 hover:bg-sand-200">
+        <Paper class="hover:bg-sand-200 rounded p-1">
           <Menu />
         </Paper>
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
-        <Paper class="p-2 rounded">
+        <Paper class="rounded p-2">
           <DropdownMenu.Item
             onSelect={() => (open = true)}
-            class="hover:bg-pampas-200 px-2 py-1 rounded"
+            class="hover:bg-pampas-200 rounded px-2 py-1"
           >
-            <Trash class="inline mr-1 h-5 align-text-bottom" />Delete
+            <Trash class="mr-1 inline h-5 align-text-bottom" />Delete
           </DropdownMenu.Item>
         </Paper>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
   </div>
-  <Paper class="w-fit py-1 px-4">
+  <Paper class="w-fit px-4 py-1">
     {project.startDate.toLocaleDateString()}
-    <MoveRight class="w-6 inline " />
+    <MoveRight class="inline w-6 " />
     {project.endDate.toLocaleDateString()}
   </Paper>
-  <Paper class="w-fit py-1 px-4">
+  <Paper class="w-fit px-4 py-1">
     {project.duration.toFixed(2)} Hours
   </Paper>
 
   <a
     href="/report/{project.id}"
-    class="py-1 bg-gray-300 hover:bg-gray-400 rounded-lg w-fit px-4 hover:underline decoration-wavy"
+    class="w-fit rounded-lg border border-stone-500 bg-gray-300 px-4 py-1 decoration-wavy hover:bg-gray-400 hover:underline"
     >export to HTML</a
   >
-  <Paper class="py-1 px-4 w-fit">
+  <Paper class="w-fit px-4 py-1">
     <form action="?/new_session" method="post">
       start a new task:
       <input
