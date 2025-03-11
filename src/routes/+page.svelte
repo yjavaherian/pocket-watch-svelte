576869ce27d26847faa77af86c1628b8f16b117f
Yousef <yousefjavaherian@gmail.com>
2025-03-11 11:48:40 +0330

    replacing page content with components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-    import { FolderPlus } from "lucide-svelte";
+    import Header from "$lib/components/header.svelte";
+    import NewProject from "$lib/components/newproject.svelte";
     import { goto } from "$app/navigation";
     import { browser } from "$app/environment";
 
@@ -10,32 +11,5 @@
     }
 </script>
 
-<h1
-    class="text-4xl flex flex-row items-end p-4 border-b font-[Doto] font-black border-stone"
->
-    <img src="/favicon.png" alt="logo" class="w-12 mr-2" />
-    Pocket watch
-</h1>
-<main
-    class="m-4 flex flex-col border border-stone bg-white rounded-lg p-10 w-fit place-self-center"
->
-    <!-- <h2 class="text-xl font-bold">
-        Pocket watch is a time tracking app that helps you keep track of your
-        time and bill your clients.
-    </h2> -->
-    <p class="text-lg mb-6">Create your first project</p>
-    <form action="?/create" method="post" class="flex flex-col">
-        <label>
-            <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="my awesome project 🤩 ..."
-                class="mb-6 p-1 w-64 border border-stone rounded-lg"
-            />
-        </label>
-        <button type="submit" class="bg-blue p-1 rounded-lg hover:bg-darkblue"
-            >New Project <FolderPlus class="ml-1 inline" />
-        </button>
-    </form>
-</main>
+<Header />
+<NewProject />
--- src/routes/project/[id]/+layout.svelte
+++ src/routes/project/[id]/+layout.svelte
@@ -1,60 +1,20 @@
 <script>
     import { page } from "$app/state";
-    import { Folder, FolderOpen, FolderPlus, Trash } from "lucide-svelte";
+    import Header from "$lib/components/header.svelte";
+    import Sidebar from "$lib/components/sidebar.svelte";
     let { children, data } = $props();
+    const projects = $derived(data.projects);
+    const activeProjectId = $derived(parseInt(page.params.id));
 </script>
 
 <div class="h-svh flex flex-col">
-    <h1
-        class="text-4xl flex flex-row items-end p-4 border-b font-[Doto] font-black border-stone"
-    >
-        <img src="/favicon.png" alt="logo" class="w-12 mr-2" />
-        Pocket watch
-    </h1>
-
+    <Header />
     <main class="flex flex-row grow">
-        <div
-            class="flex flex-col justify-between pr-4 m-4 border-r border-stone"
-        >
-            <section class="flex flex-col text-lg capitalize gap-2">
-                {#each data.projects as project}
-                    <a
-                        href="/project/{project.id}/"
-                        class="flex flex-row items-center gap-2 border border-stone rounded-lg py-1 px-2 {(page
-                            .params.id ?? null) == project.id.toString()
-                            ? 'bg-blue'
-                            : 'bg-white'}"
-                    >
-                        {#if (page.params.id ?? null) == project.id.toString()}
-                            <FolderOpen class="inline h-6 text-darkblue" />
-                        {:else}
-                            <Folder class="inline h-6 text-stone" />
-                        {/if}
-
-                        {project.name}
-                    </a>
-                {/each}
-            </section>
-            <section>
-                <form
-                    action="?/create"
-                    method="post"
-                    class=" flex flex-row items-center p-2 border border-stone rounded-lg bg-white"
-                >
-                    <label>
-                        <input
-                            type="text"
-                            name="name"
-                            id="name"
-                            class="w-56"
-                            placeholder="project name ..."
-                            autocomplete="off"
-                        />
-                    </label>
-                    <button type="submit" class=""><FolderPlus /></button>
-                </form>
-            </section>
-        </div>
+        <Sidebar
+            {activeProjectId}
+            {projects}
+            class="pr-4 m-4 border-r border-stone-500"
+        />
         <div class="p-4">
             {@render children()}
         </div>
