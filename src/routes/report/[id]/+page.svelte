097b881db4790c1c136a942aa783113df7890652
Yousef <yousefjavaherian@gmail.com>
2025-03-11 11:50:10 +0330

    improving the ui of report page

--- src/routes/report/[id]/+page.svelte
+++ src/routes/report/[id]/+page.svelte
@@ -1,70 +1,66 @@
 <script lang="ts">
     const { data } = $props();
-    const project = $derived(data.project);
+    const project = data.project;
     const blocks = $derived(
         project.sessions
             .map((s) => s.blocks.map((b) => ({ ...b, desc: s.desc })))
             .flat(),
     );
+    const border = "border border-stone-500";
 </script>
 
-<main class="m-8">
-    <header class="border-b border-stone flex flex-col pb-2">
-        <h1 class="text-2xl font-bold">Time Log Report</h1>
-        <h2 class="text-lg">Project: {project.name}</h2>
-        <h2 class="text-lg">
-            project ran from {project.startDate.toLocaleDateString()} to {project.endDate.toLocaleDateString()}.
-        </h2>
-        <h2 class="text-lg">
-            total hours: {project.duration.toFixed(2)}
-        </h2>
+<main class="w-fit m-8 font-[Helvetica]">
+    <header class={border}>
+        <h1 class="text-center bg-pampas-200 p-2 font-semibold">
+            Project Info
+        </h1>
+        <section class="p-2">
+            <h2><span class="font-semibold">Project:</span> {project.name}</h2>
+            <h2>
+                <span class="font-semibold">time line: </span>
+                {project.startDate.toLocaleDateString()} to {project.endDate.toLocaleDateString()}.
+            </h2>
+            <h2>
+                <span class="font-semibold"> total hours:</span>
+                {project.duration.toFixed(2)}
+            </h2>
+        </section>
     </header>
-    <table class="mt-8 border-y">
-        <thead class="text-left bg-pampas">
+    <table class="mt-8">
+        <thead class="bg-pampas-200">
             <tr>
-                <th class="w-32 border-stone px-1 py-1 border">Date</th>
-                <th class="w-32 border-stone px-1 py-1 border">Start Time</th>
-                <th class="w-32 border-stone px-1 py-1 border">End Time</th>
-                <th class="w-32 border-stone px-1 py-1 border"
-                    >Duration (min)</th
-                >
-                <th class="w-96 border-stone px-1 py-1 border">Description</th>
+                <th class="p-2 {border}">Date</th>
+                <th class="p-2 {border}">Start Time</th>
+                <th class="p-2 {border}">End Time</th>
+                <th class="p-2 {border}">Duration (min)</th>
+                <th class="p-2 {border}">Description</th>
             </tr>
         </thead>
         <tbody>
             {#each blocks as block}
                 <tr>
-                    <td class="border border-stone px-2 py-1"
+                    <td class="{border} px-2 py-1"
                         >{block.date &&
                             new Date(block.date).toLocaleDateString()}</td
                     >
-                    <td class="border border-stone px-2 py-1"
+                    <td class="{border} px-2 py-1"
                         >{new Date(block.start).toLocaleTimeString("en-GB", {
                             hour: "2-digit",
                             minute: "2-digit",
                         })}</td
                     >
-                    <td class="border border-stone px-2 py-1"
+                    <td class="{border} px-2 py-1"
                         >{new Date(block.end).toLocaleTimeString("en-GB", {
                             hour: "2-digit",
                             minute: "2-digit",
                         })}</td
                     >
-                    <td class="border border-stone px-2 py-1"
+                    <td class="{border} px-2 py-1"
                         >{((block.duration ?? 0) * 60).toFixed(2)}</td
                     >
-                    <td class="border border-stone px-2 py-1">{block.desc}</td>
+                    <td class="{border} px-2 py-1">{block.desc}</td>
                 </tr>
             {/each}
         </tbody>
     </table>
-
-    <!-- <h2 class="mt-4 text-2xl">Billing Info</h2>
-    <section class="mt-2 flex flex-row gap-20 text-lg">
-        <p>total hours: {project.duration.toFixed(2)}</p>
-        <p>billing rate: {project.billingRate}</p>
-        <p class="">
-            total invoice: {project.invoiceTotal.toFixed(2)}
-        </p>
-    </section> -->
 </main>
